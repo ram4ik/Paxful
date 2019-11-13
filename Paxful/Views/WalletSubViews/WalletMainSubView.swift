c4d6b0f20da2663bad0f87474a0d15fc70c19cf6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-13 12:45:13 +0200

    walletView main skeleton

--- Paxful/Views/WalletSubViews/WalletMainSubView.swift
+++ Paxful/Views/WalletSubViews/WalletMainSubView.swift
@@ -10,7 +10,77 @@ import SwiftUI
 
 struct WalletMainSubView: View {
     var body: some View {
-        Text("Wallet View")
+        ZStack {
+            VStack {
+                Image(systemName: "cube.box")
+                    .padding()
+                Text("0 BTC")
+                    .padding()
+                Text("0.00 EUR")
+                    .padding()
+                HStack {
+                    VStack {
+                        Button(action: {
+                            
+                        }) {
+                            ZStack {
+                                Circle()
+                                    .foregroundColor(Color.pink)
+                                    .frame(width: 55, height: 55)
+                                Image(systemName: "icloud.and.arrow.down")
+                                    .foregroundColor(Color.white)
+                            }
+                        }.padding()
+                        Text("BUY")
+                    }.padding()
+                    Spacer()
+                    VStack {
+                        Button(action: {
+                            
+                        }) {
+                            ZStack {
+                                Circle()
+                                    .foregroundColor(Color.green)
+                                    .frame(width: 55, height: 55)
+                                Image(systemName: "icloud.and.arrow.up")
+                                    .foregroundColor(Color.white)
+                            }
+                        }.padding()
+                        Text("SPEND")
+                    }.padding()
+                    Spacer()
+                    VStack {
+                        Button(action: {
+                            
+                        }) {
+                            ZStack {
+                                Circle()
+                                    .foregroundColor(Color.blue)
+                                    .frame(width: 55, height: 55)
+                                Image(systemName: "paperplane")
+                                    .foregroundColor(Color.white)
+                            }
+                        }.padding()
+                        Text("SEND")
+                    }.padding()
+                }
+                
+                Button(action: {
+                    
+                }) {
+                    HStack {
+                        Image(systemName: "qrcode")
+                        Text("Receive Bitcoin")
+                    }
+                }.padding()
+                Text("TRANSACTIONS")
+                    .padding()
+                Text("Get started by buying bitcoin on Paxful")
+                    .padding()
+                Text("You can buy bitcoin directly from other people using over 300 payment options")
+                    .padding()
+            }
+        }
     }
 }
 
