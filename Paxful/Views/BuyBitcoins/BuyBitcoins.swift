1068c37d63d4125bdab29fa41181fd7711f403df
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-03 09:54:37 +0300

    preparations

---
+++ Paxful/Views/BuyBitcoins/BuyBitcoins.swift
@@ -0,0 +1,21 @@
+//
+//  BuyBitcoins.swift
+//  Paxful
+//
+//  Created by ramil on 03.06.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct BuyBitcoins: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BuyBitcoins_Previews: PreviewProvider {
+    static var previews: some View {
+        BuyBitcoins()
+    }
+}
---
+++ Paxful/Views/BuyBitcoins/NetworkManager/NetworkManager.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkManager.swift
+//  Paxful
+//
+//  Created by ramil on 03.06.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import Foundation
+
+
